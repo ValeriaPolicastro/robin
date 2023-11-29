dcded3f72e1118fd9678e2f5984366f8e5eaa5e8
Dario <dario.righelli@gmail.com>
2023-11-29 10:57:40 +0100

    fixing documentation

--- R/ROBIN.R
+++ R/ROBIN.R
@@ -691,7 +691,7 @@ robinRobust <- function(graph, graphRandom,
 #' @examples 
 #' my_file <- system.file("example/football.gml", package="robin")
 #' graph <- prepGraph(file=my_file, file.format="gml")
-#' robinCompare(graph=graph, method1="louvain", args1 = list(resolution=0.8),
+#' robinCompare(graph=graph, method1="louvain", args1=list(resolution=0.8),
 #'             method2="leiden", args2=list(objective_function ="modularity"), 
 #'             measure="vi", type="independent")
 robinCompare <- function(graph, 
